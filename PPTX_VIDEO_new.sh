1f07a49ad30c1f4b1c0a2e256d6fdd1ba6ab331e
durgabhavani.v <durgabhavani.v@zohocorp.com>
2020-12-03 13:28:44 +0530

    new6

--- PPTX_VIDEO_new.sh
+++ PPTX_VIDEO_new.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 Input_path=$1
+Output_path=$2
+filename=$3
 
 Help()
 {
@@ -16,10 +18,10 @@ Help()
 
 }
 
-java -jar ./PPTXToImage/build/libs/pptxtoimage-0.1.jar ./input/ ./output/
+java -jar ./PPTXToImage/build/libs/pptxtoimage-0.1.jar $Input_path $Output_path
 
 # Using CPU
-ffmpeg -framerate 100 -i ./output/base.png -framerate 0.25 -i ./output/$Input_path/ppt_image_%03d.png -c:v libx264 -filter_complex "overlay=x='(W-w)/2':y='(H-h)/2'" -pix_fmt yuv420p -y ./output/$Input_path/output.mp4
+ffmpeg -framerate 100 -i $Output_path/base.png -framerate 0.25 -i $Output_path/$filename/ppt_image_%03d.png -c:v libx264 -filter_complex "overlay=x='(W-w)/2':y='(H-h)/2'" -pix_fmt yuv420p -y $Output_path/$filename/output.mp4
 
 # Using CPU
 # fmpeg -framerate 100 -i /home/local/ZOHOCORP/durga-7043/ppt_to_video/output/base.png -framerate 0.25 -i /home/local/ZOHOCORP/durga-7043/ppt_to_video/output/$Input_path/ppt_image_%03d.png -c:v libx264 -filter_complex "overlay=x='(W-w)/2':y='(H-h)/2'" -pix_fmt yuv420p -y /home/local/ZOHOCORP/durga-7043/ppt_to_video/output/$Input_path/output.mp4
